94b14b9e74b1bce3d1f7876426bb1e4c2a6078bc
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2022-12-28 15:54:15 +0100

    add stuff

--- components/Card.js
+++ components/Card.js
@@ -7,7 +7,7 @@ export default function Card() {
         <div class="relative flex flex-col md:flex-row md:space-x-5 space-y-3 md:space-y-0 rounded-xl shadow-lg p-3 max-w-xs md:max-w-3xl mx-auto border border-white bg-white">
           <div class="w-full md:w-1/3 bg-white grid place-items-center">
             <img
-              src="https://images.pexels.com/photos/4381392/pexels-photo-4381392.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+              src="/images/image-product-desktop.jpg"
               alt="tailwind logo"
               class="rounded-xl"
             />
